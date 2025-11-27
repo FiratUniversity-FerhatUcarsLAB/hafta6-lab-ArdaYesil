a2481ff67961399f5638ceb9c4d6b76b2e44e727
ArdaYesil <250542013@firat.edu.tr>
2025-11-27 23:44:37 +0300

    Update Proje3_RestoranSiparis.java

--- Proje3_RestoranSiparis.java
+++ Proje3_RestoranSiparis.java
@@ -5,6 +5,9 @@ görev 3 : %63 tamamlandı
 
 
 */
+
+import java.util.Scanner;
+
 public class RestorantSiparişSistemi {
     final static double comboIndirim=0.15,içecekIndirim=0.20,ogrenciIndirim=0.10;
     public static double anaYemekSeçim(int yemekDeğer){//Ana Yemeğin fiyatını belirler
@@ -81,8 +84,8 @@ public class RestorantSiparişSistemi {
         return false;
     }
 
-    public static boolean ogrenciKontrol(int gün,char ögrenci){
-        if(gün<=5 && ögrenci=='E' || ögrenci=='e')
+    public static boolean ogrenciKontrol(int gün,String ögrenci){
+        if(gün<=5 && (ögrenci=="E" || ögrenci=="e"))
             return true;
         return false;
     }
@@ -107,7 +110,80 @@ public class RestorantSiparişSistemi {
         return sonFiyat*0.10;
     }
 
+    @SuppressWarnings("StringEquality")
     public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int anaYemek,başlangiç,içecek,tatli,saat,gün;
+        double anaYemekSeçim,başlangiçSeçim,içecekSeçim,tatliSeçim,günSeçim,toplamFiyat,hesapFiyat,bahşiş;
+        boolean comboIndirim,saatSeçim,ogrenciSeçim;
+        String ogrenci;
+
+        System.out.println("====== Ana Yemek =======");
+        System.out.println("1- Izgara Tavuk - 85 TL");
+        System.out.println("2- Adana Kebap - 120 TL");
+        System.out.println("3- Levrek - 110 TL");
+        System.out.println("4- Mantı - 65 TL");
+        System.out.print("Ana Yemek (1-4) : ");
+        anaYemek = input.nextInt();
+
+        System.out.println("\n====== Başlangiç =======");
+        System.out.println("0- Hişbir şey");
+        System.out.println("1- Çorba - 25 TL");
+        System.out.println("2- Humus - 45 TL");
+        System.out.println("3- Sigara Böreği - 55 TL");
+        System.out.print("Başlangıç (0-3) : ");
+        başlangiç=input.nextInt();
+
+
+        System.out.println("\n====== İçecekler =======");
+        System.out.println("0- Hişbir şey");
+        System.out.println("1-  Kola - 15 TL");
+        System.out.println("2- Ayran - 12 TL");
+        System.out.println("3- Taze Meyve Suyu - 35 TL");
+        System.out.println("4-  Limonata - 25 TL");
+        System.out.print("İçecek (0-4) : ");
+        içecek=input.nextInt();
+
+        System.out.println("\n====== Tatlılar =======");
+        System.out.println("0- Hiçbir şey");
+        System.out.println("1- Künefe - 65 TL");
+        System.out.println("2- Baklava - 55 TL");
+        System.out.println("3- Sütlaç - 35 TL");
+        System.out.print("Tatlı (0-3) : ");
+        tatli=input.nextInt();
+
+        System.out.print("\nSaat (8-23) : " );
+        saat=input.nextInt();
+
+        System.out.print("\nÖğrenci misiniz (E/H) : " );
+        ogrenci=input.next();
+
+        System.out.println("\n====== Gün =======");
+        System.out.println("1-Pazartesi");
+        System.out.println("2-Salı");
+        System.out.println("3-Çarşamba");
+        System.out.println("4-Perşembe");
+        System.out.println("5-Cuma");
+        System.out.println("6-Cumartesi");
+        System.out.println("7-Pazari");
+        System.out.print("Gün (1-7) : ");
+        gün=input.nextInt();
+
+        anaYemekSeçim=anaYemekSeçim(anaYemek);
+        başlangiçSeçim=BaşlangiçSeçim(başlangiç);
+        içecekSeçim=içeçekSeçim(içecek);
+        tatliSeçim=tatliSeçim(tatli);
+        comboIndirim=comboKontrol(anaYemekSeçim, içecekSeçim, tatliSeçim);
+        saatSeçim=happyTimeKontrol(saat);
+        ogrenciSeçim=ogrenciKontrol(gün, ogrenci);
+        toplamFiyat=anaYemekSeçim+başlangiçSeçim+içecekSeçim+tatliSeçim;
+        hesapFiyat= fiyatIndirimHesapla(toplamFiyat, comboIndirim, ogrenciSeçim, saatSeçim, içecekSeçim);
+        bahşiş=bahşişHesapla(hesapFiyat);
+
+        System.out.println("\n=== Hesap Bilgi ===");
+        System.out.printf("Ara Toplam : %.2f TL", toplamFiyat);
+        System.out.printf("\nToplam Tutar : %.2f TL" , hesapFiyat);
+        System.out.printf("\nBahşiş Önerisi : %.2f TL",bahşiş); 
         
     }
 }
